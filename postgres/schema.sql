9f913c27cceac0a0860430c829c869f97156cf53
HarshTyagi0255 <86875256+HarshTyagi0255@users.noreply.github.com>
2026-02-06 16:11:16 +0530

    Added Tables - Customer, Order and Feedback

---
+++ postgres/schema.sql
@@ -0,0 +1,20 @@
+CREATE TABLE customers (
+  id INTEGER PRIMARY KEY,
+  first_name VARCHAR(100),
+  last_name VARCHAR(100),
+  email VARCHAR(255),
+  address JSON
+);
+
+CREATE TABLE orders (
+  id INTEGER PRIMARY KEY,
+  customer_id INTEGER REFERENCES customers(id),
+  status VARCHAR(50)
+);
+
+CREATE TABLE feedback (
+  id INTEGER PRIMARY KEY,
+  order_id INTEGER UNIQUE REFERENCES orders(id),
+  feedback_comment TEXT,
+  rating INTEGER
+);
